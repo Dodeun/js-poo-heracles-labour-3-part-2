a1df42096b2ce73d0fcef89345d1d8c81f36b2fa
Burdon Jordan <burdon.jordan@gmail.com>
2025-06-02 10:17:58 +0200

    Heracles 3 part 2

--- src/Hero.js
+++ src/Hero.js
@@ -1,13 +1,19 @@
 class Hero extends Fighter {
-  constructor(name, strength, dexterity, image, x, y) {
-    super(name, strength, dexterity, image, x, y);
-  }
+    constructor(name, strength, dexterity, image, x, y) {
+        super(name, strength, dexterity, image, x, y);
+    }
 
-  getDamage() {
-    return this.weapon ? this.strength + this.weapon.damage : this.strength;
-  };
+    getDamage() {
+        return this.weapon ? this.strength + this.weapon.damage : this.strength;
+    }
 
-  getDefense() {
-    return this.shield ? this.dexterity + this.shield.protection : this.dexterity;
-  };
+    getDefense() {
+        return this.shield
+            ? this.dexterity + this.shield.protection
+            : this.dexterity;
+    }
+
+    getRange() {
+        return this.weapon ? this.range + this.weapon.range : this.range;
+    }
 }
